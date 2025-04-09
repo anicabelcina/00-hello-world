11483b81ef8476a7d21496cb172fdeab3900d507
Anica Belcina <anicabelcina@gmail.com>
2025-04-09 22:48:55 +0800

    Update 00-0-hello-world.js

--- src/00-0-hello-world.js
+++ src/00-0-hello-world.js
@@ -1,14 +1,7 @@
 function greet() {
-  // Your Code Here
-  // input: none
-  // output: returns the string "Hello, World!"
   return 'Hello, World!';
 }
 
 function logWhatsUp() {
-  // Your Code Here
-  // input: none
-  // output: none
-  // side effect: logs the string "What's up?" to the console
   console.log('What\'s up?')
 }
